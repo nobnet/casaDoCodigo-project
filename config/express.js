56cdfe4662f6e001ae56735c2ed67e1081d7b4c7
nnobnet <nnobnet@gmail.com>
2017-06-15 18:07:50 -0300

    Página após a adição do último teste e adição da rota da home e inclusão dos arquivos da casadocodigo

--- config/express.js
+++ config/express.js
@@ -7,7 +7,8 @@ var expressValidator = require('express-validator');
 module.exports = function() {
 
     var app = express();
-
+    
+    app.use(express.static('./app/public'));
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
     
