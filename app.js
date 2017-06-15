b3ba48f4ac5da29efd9db367198722636202c864
nnobnet <nnobnet@gmail.com>
2017-06-15 19:16:37 -0300

    novo app.js. Mostra novas informações

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var io = require('socket.io')(http);
 
 app.set('io',io);
 
-http.listen(3000, function(){
-    console.log("servidor rodando");
+var porta = process.env.PORT || 3000;
+var server = http.listen(porta, function () {
 
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Servidor rodando em: http://%s:%s', host, port);
 });
