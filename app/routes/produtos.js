571952d6d818997ffcdc1976cb4c32aab518cc1b
nnobnet <nnobnet@gmail.com>
2017-06-15 09:35:23 -0300

    commit inicial. Antes do uso do mocha

---
+++ app/routes/produtos.js
@@ -0,0 +1,74 @@
+module.exports = function(app){
+    
+    var listaProdutos = function(req,res){
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.lista(function(erros,resultados){
+        res.format({
+            html: function(){
+                res.render('produtos/lista',{lista:resultados});
+            },
+            json: function(){
+                console.log('Json ok');
+                res.json(resultados)
+            }
+        });
+
+    });
+    connection.end();
+    };
+    
+    app.get('/produtos',listaProdutos);
+    
+    app.get('/produtos/form',function(req,res){
+        res.render('produtos/form',{errosValidacao:{},produto:{}});
+    });
+
+    app.post('/produtos',function(req,res){
+        var produto = req.body;
+        
+        //var validatorTitulo = req.assert('titulo','Título é obrigatório');
+        //validatorTitulo.notEmpty();
+        
+        req.assert('titulo','Título é obrigatório').notEmpty();
+        req.assert('preco','Formato inválido').isFloat()
+
+        var erros = req.validationErrors();
+        if(erros){
+            res.render('produtos/form',{errosValidacao:erros,produto:produto});
+            return;
+        }
+        
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto,function(erros,resultados){
+            console.log(erros);
+                res.redirect('/produtos');
+        });
+        
+        connection.end();
+        
+    });
+    
+/*
+app.get('/produtos',function(req,res){
+    console.log('chegou aqui');
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.lista(function(erros,resultados){
+        res.format({
+            html: function(){
+                res.render('produtos/lista',{lista:resultados});
+            },
+            json: function(){
+                console.log('Json ok');
+                res.json(resultados)
+            }
+        });
+
+    });
+    connection.end();
+});
+*/
+
+}
