7dc890adcd4b0c0f3432a69318bd83c11e722397
nnobnet <nnobnet@gmail.com>
2017-06-17 08:51:44 -0300

    Corrigindo produtosDAO.js com a tabela correta, pois em dev é Livros, mas em produção é produtos

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -3,11 +3,23 @@ function ProdutosDAO(connection){
 }
 
 ProdutosDAO.prototype.lista = function(callback){
-    this._connection.query('select * from Livros',callback);
+    if(!process.env.NODE_ENV) {
+        this._connection.query('select * from Livros',callback);
+    }
+    
+    if(process.env.NODE_ENV == 'production') {
+        this._connection.query('select * from produtos',callback);        
+    }
 }
 
 ProdutosDAO.prototype.salva = function(produto,callback){
-    this._connection.query('insert into Livros set ?',produto,callback);
+    if(!process.env.NODE_ENV) {
+        this._connection.query('insert into Livros set ?',produto,callback);
+    }
+    
+    if(process.env.NODE_ENV == 'production') {
+        this._connection.query('insert into produtos set ?',produto,callback);    
+    }
 }
 
 
