c9617aa6201fbfbbb8af020ff005b04eb5c9aab6
nnobnet <nnobnet@gmail.com>
2017-06-17 09:48:12 -0300

    Alterando o controller home para passar os erros para o fluxo do express

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,6 +3,10 @@ module.exports = function(app)  {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.lista(function(erros,resultados){
+            if(erros){
+                return next(erros);
+            }
+
             res.render('home/index',{livros:resultados});
         });
         connection.end();    
